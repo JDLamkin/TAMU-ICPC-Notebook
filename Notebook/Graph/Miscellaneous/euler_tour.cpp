bb768618f6c322e5defeb4e84bc7af1836228ac3
Celdir <michael.earl7769@gmail.com>
2018-01-07 22:30:00 -0600

    Hierholzer's algorithm for Euler Tour

---
+++ Notebook/Graph/Miscellaneous/euler_tour.cpp
@@ -0,0 +1,26 @@
+/* Hierholzer's algorithm finds an Euler circuit in a DAG.
+ * Running time: O(V + E)
+ */
+
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+typedef vector<map<int, int>> graph;
+
+void dfs(graph &g, vector<int> &circuit, int cur) {
+    for (auto& p : g[cur]) {
+        g[cur].erase(p.first);
+        dfs(g, circuit, p.first);
+    }
+    circuit.push_back(cur);
+}
+
+vector<int> euler(graph& g) {
+    vector<int> circ;
+    dfs(g, circ, 0);
+    reverse(circ.begin(), circ.end());
+    return circ;
+}

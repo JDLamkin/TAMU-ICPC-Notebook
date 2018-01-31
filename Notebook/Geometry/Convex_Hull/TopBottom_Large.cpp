9e09d9275905e655bd93bb64bdf5ebe897c2f56c
EvModder <evmodder@gmail.com>
2018-01-31 16:18:52 -0600

    fixes

--- Notebook/Geometry/Convex_Hull/TopBottom_Large.cpp
+++ Notebook/Geometry/Convex_Hull/TopBottom_Large.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 typedef long double ld;
 #define inf numeric_limits<ld>::max()
@@ -9,17 +10,24 @@ struct Point{
 		return a.x == b.x && a.y == b.y;
 	}
 	ld slope(Point& b){
-		return x == b.x ? inf : (b.y - y)/(b.x - x);
+		return (b.y - y)/(b.x - x);
 	}
 };
 //returns c_hull edge from [it, end)
 template<class It> vector<Point> half_hull(It it, It end){
 	vector<Point> hull;
-	hull.push_back(*it); hull.push_back(*(++it));
+	hull.push_back(*it);
+	while(it != end && it->x == hull[0].x) ++it;
+	if(it == end) return hull;
+	hull.push_back(*it);
+
 	while(++it != end){
 		if(it->x == hull.back().x) continue;
-		auto back2 = hull[hull.size()-2];
-		while(back2.slope(hull.back()) <= back2.slope(*it)) hull.pop_back();
+		while(hull.size() > 1){
+			auto back2 = hull[hull.size()-2];
+			if(back2.slope(*it) > back2.slope(hull.back())) hull.pop_back();
+			else break;
+		}
 		hull.push_back(*it);
 	}
 	return hull;
@@ -33,8 +41,16 @@ vector<Point> convex_hull(vector<Point>& p){
 	//get two halves of c_hull
 	vector<Point> upper = half_hull(p.begin(), --p.end());
 	vector<Point> lower = half_hull(p.rbegin(), --p.rend());
-
 	//merge & return
 	upper.insert(upper.end(), lower.begin(), lower.end());
 	return upper;
+}
+
+int main(){
+	//vector<Point> pts = {{0,1}, {0,0}, {1,1}, {1,0}};
+	vector<Point> pts = {{0,2},{0,0},{2,2,},{2,0},{1,0}};
+	vector<Point> c_hull = convex_hull(pts);
+	for(Point p : c_hull){
+		cout << '(' << p.x << ',' << p.y << ')' << endl;
+	}
 }

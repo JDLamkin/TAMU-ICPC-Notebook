a254aa747d9f41ac84bb06229ea4f4b86b8394ed
Celdir <michael.earl7769@gmail.com>
2018-02-10 21:53:20 -0600

    fixed slowness of top sort

--- Notebook/Graph/Topological_Sort/top_sort.cpp
+++ Notebook/Graph/Topological_Sort/top_sort.cpp
@@ -13,12 +13,12 @@ typedef vector<map<int, int>> graph;
 
 bool cycle = false;
 
-vector<int> top_sort(graph &in, graph &out) {
+vector<int> top_sort(vector<int> &in, graph &out) {
     vector<int> order;
     queue<int> next;
 
     for (int i = 0; i < in.size(); ++i) {
-        if (in[i].empty()) next.push(i);
+        if (in[i] == 0) next.push(i);
     }
 
     while (!next.empty()) {
@@ -26,15 +26,15 @@ vector<int> top_sort(graph &in, graph &out) {
         order.push_back(cur);
         for (auto e : out[cur]) {
             int v = e.first;
-            in[v].erase(cur);
-            if (in[v].empty()) {
+            --in[v];
+            if (in[v] == 0) {
                 next.push(v);
             }
         }
     }
 
     for (int i = 0; i < in.size(); ++i) {
-        if (!in[i].empty()) {
+        if (in[i] > 0) {
             cycle = true;
             break;
         }

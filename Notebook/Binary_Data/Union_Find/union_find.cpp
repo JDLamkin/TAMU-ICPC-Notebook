ca69eac8e0fa3fec86ff6bd14505bd839292ddf6
Celdir <michael.earl7769@gmail.com>
2018-03-07 16:30:56 -0600

    fixed my union find

--- Notebook/Binary_Data/Union_Find/union_find.cpp
+++ Notebook/Binary_Data/Union_Find/union_find.cpp
@@ -2,38 +2,35 @@
 using namespace std;
 
 struct disjoint_set{
-	vector<int> parent, size;
+    vector<int> parent;
 
-	~disjoint_set() = default;
-	disjoint_set() = delete;
-	disjoint_set(int n) : parent(n), size(n){
-		for (int i=0; i<n; ++i) {
-			parent[i] = i;
-			size[i] = 1;
-		}
-	}
+    ~disjoint_set() = default;
+    disjoint_set() = delete;
+    disjoint_set(int n) : parent(n) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
 
-	int root(int pos){
-		while(parent[pos] != pos){
-			parent[pos] = parent[parent[pos]];
-			pos = parent[pos];
-		}
-		return pos;
-	}
+    int root(int pos) {
+        int tmp = pos;
+        while (parent[tmp] != tmp) {
+            tmp = parent[tmp];
+        }
+        while (pos != tmp) {
+            int next = parent[pos];
+            parent[pos] = tmp;
+            pos = next;
+        }
+        return tmp;
+    }
 
-	bool find(int p, int q){
-		return root(p) == root(q);
-	}
+    bool find(int p, int q) {
+        return root(p) == root(q);
+    }
 
-	void merge(int p, int q){
-		int rootp = root(p), rootq = root(q);
-		if(size[rootp] > size[rootq]){
-			parent[rootq] = rootp;
-			size[rootp] += size[rootq];
-		}
-		else{
-			parent[rootp] = rootq;
-			size[rootq] += size[rootp];
-		}
-	}
+    void merge(int p, int q) {
+        int rootp = root(p), rootq = root(q);
+        parent[rootp] = rootq;
+    }
 };

9ff530c6840663222617d7c15e976e8b695858bc
Celdir <michael.earl7769@gmail.com>
2018-11-05 19:36:48 -0600

    fixed geom

--- Notebook/Geometry/Convex_Hull/Graham_Large.cpp
+++ Notebook/Geometry/Convex_Hull/Graham_Large.cpp
@@ -5,6 +5,7 @@ vpt hull(vpt& pts) {
 	for(pnt& pt : pts) pt = pt - pvt;
 	sort(pts.begin(), pts.end(), cnvaCmp);
 	for(pnt& pt : pts) pt = pt + pvt;
+    vpt h;
 	for(pnt pt : pts) {
 		while(h.size() > 1 && cross(pt - BCK(h, 1), BCK(h, 1) - BCK(h, 2)) >= 0) h.pop_back();
 		h.push_back(pt);
